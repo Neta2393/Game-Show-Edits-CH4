d487963fecffedf28246e69dfeea0d212c31d2d6
Neta2393 <benetheahardin@yahoo.com>
2023-04-27 20:13:16 -0700

    added more context to javaScript file and README.md

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ The contents of the repo include an assets folder that includes a script.js file
 ## Styling/Creation
 This repo started with the creation of unique repo name. While working on the style.css and the script.js, I found ways to enhance the index.html file. Within my time at bootcamp I have learned how to create buttons within the index.html. Using the script.js I worked to make the buttons that I created in the index.html work and have a function within the buttons. All of the files and codes started with a blank page and I used help from past lessons and web sources to ensure that the functions worked as I wanted them to. Within the style.css file I worked to ensure the container 
 
+## Function
+When you arrive on the webpage you will see a Start button. When you click the start button you will be given the first of many questions. When you click the correct answer the contents of that answer button will turn yellow. If you click the incorrect answer the contents will remain white. When you click the next button you will be taken to the next question and the same thing will loop until you hit the end of the questions. Once you get to the final question there will be a restart button that will bbegin the quiz all over again.
+
+## Trial and Error
+While working on the following repo I came across some obstacles , but as I am learning in coding bootcamp just because it is a struggle now doesnt mean it will be later on. I am learning to rely on my resources and learning how to get better as time goes on. Now I can more accurately see the mistakes that I have made in the code. At times It is hard and frustrating when I cant get something, but I have no thought to quit learning. I hope that you can see the growth I have made in the following repo and I hope you enjoy it. 
 
 
 
--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,140 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-const questionElement = document.getElementById("question")
-const answerButtonElement = document.getElementById("answer-button")
-const timerElement = document.querySelector(".timer-count")
+// Defined the startButton, nextButton, questionContainerElement, questionElement, and answerButtonsElement
+// Due to having different elements that I wanted to function correctly in the code I opted to use const instead of let or var
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-let randomQuestions, currentQuestionIndex
-// Created an event listener(callback) function for the start button
-startButton.addEventListener("click", startGame)
-
-
-function startGame(){
-  console.log("start")
-  startButton.classList.add("hide")
-  randomQuestions = question.sort(() => Math.random() - 0)
+// The next block of code is have the questions appear in a random order and not go from top to bottom or bottom to top
+// The currentQuestionIndex is placed to ensure that the shuffled questions are coming from the currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
+// Added event listener to call back the function of the startButton
+startButton.addEventListener('click', startGame)
+// Added event listener to call back the function of the next button
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+// Created a function tell the files what to do when the start button is clicked I am asking it to pick a random question and hide the rest of the question
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .6)
   currentQuestionIndex = 0
-  questionContainerElement.classList.remove("hide")
-  setnextQuestion()
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function setnextQuestion(){
-  showQuestion(randomQuestions[currentQuestionIndex])
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
-     const button = document.createElement("button")
-     button.innerText = answer.text
-     button.classList.add("btn")
-     if (answer.correct) {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
       button.dataset.correct = answer.correct
-     }
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
   })
-
 }
 
-function selectAnswer() {
-
-}
-
-const question = [
-  {
-    question: "what is the dogs name on Scooby Doo?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "33", correct: false }
-    ]
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
-]
-
-
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
 }
+// This block of code sets a function for selecting an answer in the quiz
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+// The following block of code is defining the questions and answers
+// Created an array to place the questions and answers so that when the start button is clicked it is choosing from the specific questions below and does not return as undefined.
+const questions = [
+  {
+    question: 'Which is not apart of the DOM tree?',
+    answers: [
+      { text: '<title>', correct: false },
+      { text: '<dominoes>', correct: true },
+      { text: '<href>', correct: false },
+      { text: '<h1>', correct: false}
+    ]
+  },
+  {
+    question: 'What year was the first computer invented?',
+    answers: [
+      { text: '1822', correct: true },
+      { text: '1981', correct: false },
+      { text: '2010', correct: false },
+      { text: '2035', correct: false }
+    ]
+  },
+  {
+    question: 'Who was the worlds first programmer?',
+    answers: [
+      { text: 'Grace Hopper', correct: false },
+      { text: 'Steve Wozniak', correct: false },
+      { text: 'Bill Gates', correct: false },
+      { text: 'Ada Lovelace', correct: true }
+    ]
+  },
+  {
+    question: 'What was the first computer virus?',
+    answers: [
+      { text: 'roaches', correct: false },
+      { text: 'virus', correct: false },
+      { text: 'creeper', correct: true },
+      { text: 'bugs', correct: false }
+    ]
+  },
+  {
+    question: 'How many program developers are there in the world?',
+    answers: [
+      { text: '500,000', correct: false },
+      { text: '4.5 million', correct: false },
+      { text: '26.9 million', correct: true },
+      { text: '3 billion', correct: false}
+    ]
+  },
+ {
+  question: 'How many weeks have I been in coding bootcamp?',
+  answers: [
+    { text: '3 days', correct: false },
+    { text: '1 month', correct: false },
+    { text: '5 weeks', correct: true },
+    { text: '3 months', correct: false }
+  ]
+ }
+]
